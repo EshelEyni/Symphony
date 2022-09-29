154130563488cf3166ce5b8bed04a09a4e2ffbb9
Eshel Eyni <esheleyni@gmail.com>
2022-09-29 09:24:46 +0300

    2 commit

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'dbURL': 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  'dbURL': 'mongodb+srv://Eshel:eshel1992@cluster0.dd3ouxy.mongodb.net/?retryWrites=true&w=majority',
 }
