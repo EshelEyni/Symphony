6ea11b80a25b9ba2af75fafcad346318e03da903
Eshel Eyni <esheleyni@gmail.com>
2022-10-23 19:43:53 +0300

    config fix

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,6 +1,8 @@
 module.exports = {
   'note': 'No actual secrets here!',
 
-  'dbURL': 'your db url should go here, not in the repository of course',
-  'sessionKey': 'your session key should go here, not in the repository of course'
+  'dbURL': process.env.MONGO_DB_URL,
+  'sessionKey': process.env.SESSION_KEY,
+  'youTubeKey': process.env.YOUTUBE_KEY
+
 }
