e3d89f1ee636e31ce7eaeda864a3e170fd1bee7c
Eshel Eyni <esheleyni@gmail.com>
2022-12-14 18:40:21 +0200

    deleted expendable files

--- backend/api/user/user.routes.js
+++ backend/api/user/user.routes.js
@@ -8,7 +8,6 @@ const router = express.Router()
 
 router.get('/', getUsers)
 router.get('/:id', getUser)
-
 router.put('/:id', requireAuth, updateUser)
 router.delete('/:id', requireAuth, deleteUser)
 
